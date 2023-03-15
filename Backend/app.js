e37ac327c729c74bee5a4487feb3894aaa6382bf
arvind <developer.arvind003@gmail.com>
2023-03-16 01:44:41 +0530

    code

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,6 @@ app.use(cors({
     origin: '*'
 }));
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
